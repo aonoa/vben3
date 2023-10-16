3f9745b5eafea1cf21750e74a2f75f3d39840590
aonoa <1991849113@qq.com>
2023-10-16 12:31:53 +0800

    fix: 添加菜单中文翻译
    Signed-off-by: aonoa <1991849113@qq.com>

--- packages/locale/src/lang/zh-CN/routes.ts
+++ packages/locale/src/lang/zh-CN/routes.ts
@@ -20,5 +20,15 @@ export default {
       treeTable: '树形表格',
     },
     modal: "模态框",
+    system: {
+      moduleName: '系统管理',
+      account: '账号管理',
+      account_detail: '账号详情',
+      password: '修改密码',
+      dept: '部门管理',
+      menu: '菜单管理',
+      role: '角色管理',
+    },
   },
+
 }
